c22571c823880efd4d80c52dbdcf602840de0532
Markus Theil <markus.theil@secunet.com>
2025-10-15 18:34:24 +0200

    chacha_rng: switch to HMAC(SHA-512)
    Signed-off-by: Markus Theil <markus.theil@secunet.com>

--- src/lib/rng/chacha_rng/chacha_rng.cpp
+++ src/lib/rng/chacha_rng/chacha_rng.cpp
@@ -28,7 +28,7 @@ ChaCha_RNG::ChaCha_RNG(RandomNumberGenerator& underlying_rng,
                        size_t reseed_interval,
                        bool fast_key_erasure) :
       Stateful_RNG(underlying_rng, reseed_interval),
-      m_hmac(MessageAuthenticationCode::create_or_throw("HMAC(SHA-256)")),
+      m_hmac(MessageAuthenticationCode::create_or_throw("HMAC(SHA-512)")),
       m_chacha(StreamCipher::create_or_throw("ChaCha(20)")),
       m_fast_key_erasure(fast_key_erasure) {
    clear();
@@ -39,7 +39,7 @@ ChaCha_RNG::ChaCha_RNG(RandomNumberGenerator& underlying_rng,
                        size_t reseed_interval,
                        bool fast_key_erasure) :
       Stateful_RNG(underlying_rng, entropy_sources, reseed_interval),
-      m_hmac(MessageAuthenticationCode::create_or_throw("HMAC(SHA-256)")),
+      m_hmac(MessageAuthenticationCode::create_or_throw("HMAC(SHA-512)")),
       m_chacha(StreamCipher::create_or_throw("ChaCha(20)")),
       m_fast_key_erasure(fast_key_erasure) {
    clear();
@@ -49,7 +49,7 @@ ChaCha_RNG::ChaCha_RNG(Entropy_Sources& entropy_sources,
                        size_t reseed_interval,
                        bool fast_key_erasure) :
       Stateful_RNG(entropy_sources, reseed_interval),
-      m_hmac(MessageAuthenticationCode::create_or_throw("HMAC(SHA-256)")),
+      m_hmac(MessageAuthenticationCode::create_or_throw("HMAC(SHA-512)")),
       m_chacha(StreamCipher::create_or_throw("ChaCha(20)")),
       m_fast_key_erasure(fast_key_erasure) {
    clear();
@@ -57,7 +57,8 @@ ChaCha_RNG::ChaCha_RNG(Entropy_Sources& entropy_sources,
 
 void ChaCha_RNG::clear_state() {
    m_hmac->set_key(std::vector<uint8_t>(m_hmac->output_length(), 0x00));
-   m_chacha->set_key(m_hmac->final());
+   const auto chacha_key = m_hmac->final();
+   m_chacha->set_key(chacha_key.data(), m_chacha->key_spec().maximum_keylength());
 }
 
 void ChaCha_RNG::generate_output(std::span<uint8_t> output, std::span<const uint8_t> input) {
@@ -69,7 +70,7 @@ void ChaCha_RNG::generate_output(std::span<uint8_t> output, std::span<const uint
 
    m_chacha->write_keystream(output);
 
-   // overwrite key after each output operation for backtracking resistance
+   // optionally overwrite key after each output operation for backtracking resistance
    if(m_fast_key_erasure) {
       const auto next_key = m_chacha->keystream_bytes(m_chacha->key_spec().maximum_keylength());
       m_chacha->set_key(next_key);
@@ -78,7 +79,8 @@ void ChaCha_RNG::generate_output(std::span<uint8_t> output, std::span<const uint
 
 void ChaCha_RNG::update(std::span<const uint8_t> input) {
    m_hmac->update(input);
-   m_chacha->set_key(m_hmac->final());
+   const auto chacha_key = m_hmac->final();
+   m_chacha->set_key(chacha_key.data(), m_chacha->key_spec().maximum_keylength());
    const auto mac_key = m_chacha->keystream_bytes(m_hmac->output_length());
    m_hmac->set_key(mac_key);
 }
